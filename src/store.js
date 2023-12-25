2180377a87e55864985fd82d41c626fa134f8a5f
pranjal.trvd <pranjaltrvd12@gmail.com>
2023-12-25 19:03:07 +0530

    frontend complete

---
+++ src/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import MenuReducer from './components/slice/menuSlice'
+import ToolboxReducer from './components/slice/toolboxSlice'
+
+export const store = configureStore({
+    reducer: {
+        menu: MenuReducer,
+        toolbox: ToolboxReducer
+    }
+})
